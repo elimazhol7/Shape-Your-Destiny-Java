40b99dbd329f4efb9491cf879e4a9eeae7183b0a
Elimka777 <elimazholdubaeva@gmail.com>
2025-09-28 23:17:12 -0500

    Update

--- app/src/test/java/org/example/isoscelesrighttriangleTest.java
+++ app/src/test/java/org/example/isoscelesrighttriangleTest.java
@@ -7,7 +7,7 @@ public class isoscelesrighttriangleTest {
     void testIsoRightTriangleAreaPerimeterSides() {
         isoscelesrighttriangle iso = new isoscelesrighttriangle(5);
         assertEquals(12.5, iso.getArea(), 0.0001);
-        assertEquals(5 + 5 + Math.sqrt(50), iso.getPerimeter(), 0.0001);
+        assertEquals(17.071, iso.getPerimeter(), 0.0001);
         assertEquals(3, iso.numberOfSides());
     }
 }
